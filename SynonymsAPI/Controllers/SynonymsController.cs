64f00eb3e1ead82645189bc920dacaa3ce36e2c8
Your Name <your@email.com>
2022-06-08 22:45:20 +0200

    prvi komit

---
+++ SynonymsAPI/Controllers/SynonymsController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SynonymsAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.InMemory;
+using Newtonsoft.Json;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SynonymsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SynonymsController : ControllerBase
+    {
+        private readonly SynonymsContext _context;
+
+        public SynonymsController(SynonymsContext context)
+        {
+            _context = context;
+        }
+
+
+        // GET: api/Synonyms
+        [HttpGet("{word}")]
+        public async Task<ActionResult<IEnumerable<string>>> GetSynonims(string word)
+        {
+            /* "subset" represents synonyms string list for the given word*/
+            var subset = _context.Synonims.Where(y => y.Word == word).Select(y => y.Synonym).ToList();
+
+            /*union data subset represents data selected by transitive rule 
+             * UnionSubset = from x in _context.Synonims where subset.Contains(x.Word) select x.Synonym;*/
+
+            return await _context.Synonims.Where(y => y.Word == word).Select(y => y.Synonym).Union(from x in _context.Synonims where subset.Contains(x.Word) select x.Synonym).ToListAsync();
+
+        }
+
+
+        // POST api/Synonyms/word/synonymsString
+        [HttpPost("{word}/{synonym}")]
+        public async Task<ActionResult<Synonyms>> Post(string word, string synonym)
+        {
+            var syn = new Synonyms();
+            syn.Word = word;
+            syn.Synonym = synonym;
+            _context.Synonims.Add(syn);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("Post", new { syn.Word, syn.Synonym }, syn);
+        }
+       
+       
+    }
+}
---
+++ SynonymsAPI/Models/Synonyms.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.InMemory;
+
+namespace SynonymsAPI.Models
+{
+    public class SynonymsContext : DbContext
+    {
+        public SynonymsContext(DbContextOptions<SynonymsContext> options) : base(options)
+        { }
+
+        public DbSet<Synonyms> Synonims { get; set; }
+
+    }
+
+    
+    public class Synonyms
+    {
+        private static int id_counter = 0;
+
+        public int Id { get; set; }
+        public string Word { get; set; }
+        public string Synonym { get; set; }
+
+        public Synonyms()
+        {
+            this.Id = System.Threading.Interlocked.Increment(ref id_counter);
+        }
+    }
+}
